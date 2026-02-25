dcf59c77143ec9f192951df060a56c6071d1d32f
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-02-25 12:00:48 +0100

    exercice1

--- exercice3/main.js
+++ exercice3/main.js
@@ -1,13 +1,6 @@
 let maBaliselabelInput = document.querySelector("#lastname");
-// console.log(maBaliselabelInput);
 
-// maBaliselabelInput.KeyboardEvent("dialog", function () {
-// //   console.log("Hello world !");
-
-//   alert();
-// });
-
-maBaliselabelInput.addEventListener("keyup", handleKeydown );
+maBaliselabelInput.addEventListener("keyup", handleKeydown);
 
 function handleKeydown() {
   alert(maBaliselabelInput.value);
